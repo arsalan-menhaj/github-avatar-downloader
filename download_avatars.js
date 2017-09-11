f23a1424bbb9b1277d499797ef9122202321610a
arsalan-menhaj <arsalan.menhaj@gmail.com>
2017-09-11 20:23:40 +0000

    Added function to download image from URL

--- download_avatars.js
+++ download_avatars.js
@@ -42,15 +42,35 @@ function getRepoContributors(repoOwner, repoName, cb) {
   */
 }
 
-
-
 function printURL(array) {
   array.forEach(function(contributor) {
     console.log('Avatar URL: ', contributor.avatar_url);
   });
 }
 
+imageURL = "https://avatars2.githubusercontent.com/u/2741?v=3&s=466";
+imagePath =  "./avatars/kvirani.jpg";
 
-getRepoContributors("jquery", "jquery", printURL);
+function downloadImageByURL(url, filePath) {
+  // ...
+  request.get(url)               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Status Message:', response.statusMessage);
+         console.log('Response Status Message:', response.headers['content-type']);
+         console.log('Downloading image...');
+       })
+       .on('end', function (response) {
+          console.log('Download complete.')
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+
+
+// getRepoContributors("jquery", "jquery", printURL);
+downloadImageByURL(imageURL,imagePath);
 
 
