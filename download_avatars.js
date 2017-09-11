8a53916eef4edbcf7b2429e122b9a2783468c212
arsalan-menhaj <arsalan.menhaj@gmail.com>
2017-09-11 21:04:44 +0000

    Finalized downloadImageFromURL so that it accepts command line arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,22 @@
+// Stores command line arguments
+var owner = process.argv[2];
+var repo = process.argv[3];
+
+// Calls 'request' and 'fs' modules
 var request = require('request');
 var fs = require('fs');
 
+// Github credentials
 var GITHUB_USER = "arsalan-menhaj";
 var GITHUB_TOKEN = "76bf6b2df263c2e27fbe71bbc91ab91f03bcf496";
 
-
+// Welcome message
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// Main function
 function getRepoContributors(repoOwner, repoName, cb) {
 
+  // Sets up URL and User-Agent for request
   var reqObject = {
     uri: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
@@ -16,14 +24,18 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   }
 
+  // GET request - parses responses as JSON and passes data to downloadImageByURL
   request.get(reqObject, function(error, response, body) {
-    if (response && response.statusCode == 200) {
+    if (error) { throw error; }
+    else if (response && response.statusCode === 200) {
       var json = JSON.parse(body);
       cb(json);
     }
   });
 }
 
+// Receives JSON data from main function, downloads pictures from avatar_url,
+// and names pictures after the corresponding contributor
 function downloadImageByURL(array) {
   array.forEach(function(contributor) {
      request.get(contributor.avatar_url)
@@ -43,4 +55,4 @@ function downloadImageByURL(array) {
      });
 }
 
-getRepoContributors("jquery", "jquery", downloadImageByURL);
+getRepoContributors(owner, repo, downloadImageByURL)
